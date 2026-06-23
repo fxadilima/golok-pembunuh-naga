4f3933e27e3ad804550cfcebf17e975931bbf190
fxadilima <xaverius2017@gmail.com>
2026-06-23 21:44:11 +0700

    Add some links

--- content/bs/ArticlesSidebar.tsx
+++ content/bs/ArticlesSidebar.tsx
@@ -3,19 +3,19 @@ const Pills = () => {
     return (
         <div class="card">
             <div class="card-header">
-                <h3>Some Links</h3>
-                <p>Lorem ipsum dolor sit ame.</p>
+                <h3>Catatan</h3>
+                <p>Link berikut ada di halaman ini juga.</p>
             </div>
             <div class="card-body">
                 <ul class="nav nav-pills flex-column">
                     <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
+                        <a class="nav-link active" href="#notes">Editorial</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#blogs">Blogs</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link disabled" href="#">Belum terpikir...</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link disabled" href="#">Disabled</a>
@@ -32,10 +32,10 @@ export default function Sidebar() {
             <div class="card my-3">
                 <img class="card-img-top" src="/images/kaifeng.jpg" alt="Me" style="width:100%" />
                 <div class="card-header">
-                    <h3>Header</h3>
+                    <h3>Kaifeng, 1357</h3>
                 </div>
                 <div class="card-body">
-                    <p>Card body</p>
+                    <p>Cerita tentang Qi Shengniang di Kaifeng 1357?</p>
                 </div>
                 <div class="card-footer">
                     <h4>Footer</h4>
